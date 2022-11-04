bf19997b646d083a63e4cb2cf003122689bde357
Eugene Ratushnyak <lemuriyskiy@rambler.ru>
2022-11-04 15:07:23 +0300

    Fix helptrigger bug

--- Assets/Scripts/Triggers/HelpTrigger.cs
+++ Assets/Scripts/Triggers/HelpTrigger.cs
@@ -19,6 +19,11 @@ public class HelpTrigger : Trigger
 
     private void OnEnable()
     {
+        
+    }
+
+    private void Start()
+    {
         _helpWindow.gameObject.transform.DOScaleX(MinImageScale, DurationChangeImage);
         _helpWindow.gameObject.transform.DOScaleX(MaxImageScale, DurationChangeImage).SetDelay(DurationChangeImage);
         _helpTMPro.text = Lean.Localization.LeanLocalization.GetTranslationText(_text);
